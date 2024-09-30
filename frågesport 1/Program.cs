6679f1b9761a75c3537c49522dbf334309b32354
joakim lundell selander <joakim.lundellselander@elev.ga.ntig.se>
2024-09-30 09:11:50 +0200

    first commit

---
+++ frågesport 1/Program.cs
@@ -0,0 +1,46 @@
+﻿int Poäng = 0;
+string val;
+
+Console.WriteLine("Välkommen till min frågesport. Du kommer få 3 frågor att svara på. Lycka till");
+Console.ReadLine();
+
+Console.WriteLine("Fråga  1, vad h eter jag i efternamn ?");
+Console.writeline
+val = Console.ReadLine();
+if (val.ToLower() == "a")
+{
+    Poäng++;
+    Console.WriteLine("RÄTT SVAR!");
+}
+else
+{
+    Console.WriteLine("FEL SVAR!");
+}
+
+
+Console.WriteLine("Fråga 2,");
+val = Console.ReadLine();
+if (val.ToLower() == "")
+{
+    Poäng++;
+    Console.WriteLine("RÄTT SVAR!");
+}
+else
+{
+    Console.WriteLine("FEL SVAR!");
+}
+
+Console.WriteLine("Fråga  3,");
+val = Console.ReadLine();
+if (val.ToLower() == "")
+{
+    Poäng++;
+    Console.WriteLine("RÄTT SVAR!");
+}
+else
+{
+    Console.WriteLine("FEL SVAR!");
+}
+
+Console.WriteLine(Poäng);
+Console.ReadLine();
